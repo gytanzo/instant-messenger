27f1ba51c4d3fee384327ff7da51dc4e72a774a0
gytanzo <bdm23@buffalo.edu>
2019-10-20 17:02:11 -0400

    Implemented two functions to verify the validity and what type of message inputs are

--- src/pack.c
+++ src/pack.c
@@ -1,9 +1,90 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 #include "serialize.h"
+
 char UBIT[NAME_SIZE] = "bdm23";
+
+int initalize(void *packed, int type){ /* Sets the first 17 elements of packed */
+    int ubitIndex = 0; /* Keeps track of where in the UBIT array I am */
+    int packedIndex = 0; /* Keeps track where in packed I am*/
+
+    *(int *)(packed + packedIndex) = type;
+    packedIndex++;
+
+    while (ubitIndex != NAME_SIZE - 1){ 
+        *(char *)(packed + packedIndex) = UBIT[ubitIndex];
+        packedIndex++;
+        ubitIndex++;
+    }
+
+    if (packedIndex != 16){
+        printf("%s%d%s\n", "Error: packedIndex has a value of", packedIndex, " when it should be 16.");
+        return 0;
+    }
+
+    else {
+        return packedIndex;
+    }
+}
+
+bool validity(char *input){
+    int length = strlen(input);
+
+    if (length > 256){
+        puts("Error: Message length exceeds MAX_MESSAGE_SIZE.");
+        return false;
+    }
+    
+    for (int i = 0; i <= length - 1; i++){
+        if (input[i] != ' '){
+            return true;
+        }
+    }
+    
+    puts("Error: Message is completely composed of ASCII space characters.");
+    return false;
+}
+
+int inputCheck(char *input) {
+    if (!validity(input)){
+        return -1;
+    }
+    
+    if (input[0] == '/' && input[1] == 'm' && input[2] == 'e'){
+        return 1; /* This is a status message. */
+    }
+
+    else if (input[0] == '@'){
+        if (input[1] == ' '){
+            puts("Error: You didn't provide a username to tag.");
+            return -1;
+        }
+        
+        int index = -1;
+        
+        for (int i = 1; i < 17; i++){
+            if (input[i] == ' '){
+                index = i;
+                i = 16;
+            }
+        }
+
+        if (index == -1){
+            puts("Error: The username you provided is too long.");
+            return -1;
+        }
+        
+        puts("This is a labeled message.");
+        return 3;
+    }
+
+    puts("Error: Reached end of inputCheck.");
+    return -1;
+}
 
 /* Pack the user input provided in input into the appropriate message
  * type in the space provided by packed.  You can assume that input is a
@@ -24,16 +105,9 @@ int pack(void *packed, char *input) {
  * Returns the message type.
  */
 int pack_refresh(void *packed, int message_id) {
-    char packageType = 0;
-    int ubitIndex = 0, packedIndex = 1;
+    char packageType = 0; /* The package type for this function will always be zero */
 
-    *(int *)(packed) = packageType;
-
-    while (ubitIndex != NAME_SIZE - 1){
-        *(char *)(packed + packedIndex) = UBIT[ubitIndex];
-        packedIndex++;
-        ubitIndex++;
-    }
+    int packedIndex = initalize(packed, packageType);
 
     *(int *)(packed + packedIndex) = message_id;
     
