04fd7b4c5808a06a8ac42ba6434d23fb9442af6c
gytanzo <bdm23@buffalo.edu>
2019-10-20 18:08:57 -0400

    Last commit of 10/20

--- src/pack.c
+++ src/pack.c
@@ -5,6 +5,7 @@
 #include <string.h>
 
 #include "serialize.h"
+#define statusHead 4 /* Used to calculate the length of a /me message. */
 
 char UBIT[NAME_SIZE] = "bdm23";
 
@@ -13,7 +14,6 @@ int initalize(void *packed, int type){ /* Sets the first 17 elements of packed *
     int packedIndex = 0; /* Keeps track where in packed I am*/
 
     *(int *)(packed + packedIndex) = type;
-    packedIndex++;
 
     while (ubitIndex != NAME_SIZE - 1){ 
         *(char *)(packed + packedIndex) = UBIT[ubitIndex];
@@ -22,7 +22,7 @@ int initalize(void *packed, int type){ /* Sets the first 17 elements of packed *
     }
 
     if (packedIndex != 16){
-        printf("%s%d%s\n", "Error: packedIndex has a value of", packedIndex, " when it should be 16.");
+        printf("%s%d%s\n", "Error: packedIndex has a value of ", packedIndex, " when it should be 16.");
         return 0;
     }
 
@@ -50,12 +50,34 @@ bool validity(char *input){
 }
 
 int inputCheck(char *input) {
+    int length = strlen(input);
+    
     if (!validity(input)){
         return -1;
     }
-    
+
     if (input[0] == '/' && input[1] == 'm' && input[2] == 'e'){
-        return 1; /* This is a status message. */
+        int check = -1;
+        
+        if (input[3] != ' '){
+            puts("Error: Invalid character following the '/me'.");
+            return -1;
+        }
+        
+        for (int i = statusHead - 1; i < length - 1; i++){
+            if (input[i] != ' '){
+                check = 0;
+            }
+        }
+        
+        if (check != 0){
+            puts("Error: Status message is completely composed of ASCII space characters.");
+            return -1;
+        }
+        
+        else {
+            return 1; /* This is a status message. */
+        }
     }
 
     else if (input[0] == '@'){
@@ -65,7 +87,6 @@ int inputCheck(char *input) {
         }
         
         int index = -1;
-        
         for (int i = 1; i < 17; i++){
             if (input[i] == ' '){
                 index = i;
@@ -78,8 +99,27 @@ int inputCheck(char *input) {
             return -1;
         }
         
-        puts("This is a labeled message.");
-        return 3;
+        return 3; /* This is a labeled message. */
+    }
+
+    else if (input[0] == '/' && input[1] == 's' && input[2] == 't' && input[3] == 'a' && input[4] == 't' && input[5] == 's'){
+        int length = strlen(input);
+        if (length > 7){
+            puts("Error: '/stats' call too long.");
+            return -1;
+        }
+        
+        if (input[6] != ' '){
+            puts("Error: Invalid character following the '/stats'.");
+            return -1;
+        }
+        else {
+            return 4; /* This is a statistics message. */
+        }
+    }
+
+    else {
+        return 2; /* This is a plain message. */
     }
 
     puts("Error: Reached end of inputCheck.");
@@ -94,6 +134,32 @@ int inputCheck(char *input) {
  * Returns the message type for valid input, or -1 for invalid input.
  */
 int pack(void *packed, char *input) {
+    int type = inputCheck(input), length;
+    int packedLoc = 18; /* After initializiation, the next element of packed will always be 18. */
+    
+    if (type == -1){
+        return -1; /* Invalid input (I would have already printed the error code, so I won't add one here). */
+    }
+
+    else if (type == 1){
+        initalize(packed, type);
+        
+        length = strlen(input) - statusHead;
+        *(size_t *)(packed + packedLoc) = length;
+        packedLoc++;
+
+        *(size_t *)(packed + packedLoc) = 0;
+        packedLoc++;
+
+        for (int i = statusHead; i <= length; i++){
+            *(char *)(packed + packedLoc) = input[i];
+            packedLoc++;
+        }
+        
+        return type;
+    }
+
+    puts("How did you get here? This is quite impressive.");
     return -1;
 }
 
